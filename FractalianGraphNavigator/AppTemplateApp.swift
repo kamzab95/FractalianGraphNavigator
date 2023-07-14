06a63b16701fe06f3a8e91412d91e0415d6100e1
Kamil Zaborowski <kamzab95@gmail.com>
2023-07-14 23:17:57 +0200

    base project setup

---
+++ FractalianGraphNavigator/AppTemplateApp.swift
@@ -0,0 +1,20 @@
+//
+// FractalianGraphNavigatorApp.swift
+// FractalianGraphNavigator
+//
+// Created by Kamil Zaborowski on 07/07/2023
+// Copyright © 2023 Kamil Zaborowski. All rights reserved.
+//
+
+import SwiftUI
+import ModularModel
+import GraphService
+
+@main
+struct FractalianGraphNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
